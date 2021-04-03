5632413609e3f6ed247fbd29fe51c853fd9a658d
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-03 14:12:09 -0300

    Exercício 2

---
+++ exercicio2/js.js
@@ -0,0 +1,16 @@
+function verify () {
+    let anoAtual = new Date().getFullYear()
+    let dataNasc = document.getElementById('n11')
+
+    if (Number(dataNasc.value) > anoAtual || dataNasc.value.lenght == 0)
+        window.alert('Erro')
+    else {
+        let genero = document.getElementsByName('radsex')[0].checked ? 'Masculino' : 'Feminino'
+        var img = 
+
+        document.getElementById('msg2').innerText = (anoAtual - dataNasc.value) 
+        + ' anos de idade e é do gênero ' + genero
+    }
+
+    
+}
