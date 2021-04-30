5806a8fb33df626c64eae8e348d7f3965a107a96
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-29 23:26:50 -0300

    Associação de objetos

--- POO/classes.js
+++ POO/classes.js
@@ -1,10 +1,35 @@
+class Animal {
+    constructor(type, behaviour){
+        this._type = type
+        this._behaviour = behaviour
+    }
+
+    get type () {
+        return this._type
+    }
+
+    get behaviour () {
+        return this._behaviour
+    }
+}
+
+
+class Pet {
+    constructor(name, age, specie){
+        this._name = name
+        this._age = age
+        this._specie = specie
+    }
+}
+
 class Person {
-    constructor(name, age, borndate, weight, height) {
+    constructor(name, age, borndate, weight, height, pet) {
         this._name = name
         this._age = age
         this._borndate = borndate
         this._weight = weight
         this._height = height
+        this._pet = pet
     }
 
     get name () {
@@ -21,5 +46,7 @@ class Person {
 
 }
 
-let obj = new Person('Mateus', 24, '30/11/1996', 59, 173)
+let obj = new Person('Mateus', 24, '30/11/1996', 59, 173, new Pet('Thor', 12, 'Dog'))
+
 console.log(obj._name)
+console.log(obj._pet._name)
