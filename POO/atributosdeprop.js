0af614c06ce3d506a147bd1100dd4bca09117490
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-05-04 03:49:19 -0300

    enumerable e configurable
    Algumas propriedades de atributos de objetos.

---
+++ POO/atributosdeprop.js
@@ -0,0 +1,24 @@
+class Person {
+    constructor (name, age) {
+        this.name = name
+        this.age = age
+    }
+    
+}
+
+let person = new Person('Mateus', 24)
+
+/* Ignora/(Des)configura atributo escolhido */
+Object.defineProperty(person, 'age', {
+    /* Ignora */
+    enumerable: false,
+    /* Torna-se inconfigurável para algumas operações com o atributo */
+    configurable: false
+})
+
+/* Não deleta pois o atributo já não está mais conifgurável, mas o valor dele pode ser modificado */
+console.log(delete person.age)
+
+
+for (atribute in person)
+    console.log(atribute)
