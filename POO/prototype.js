dcca182ee042496ee492affde562725731bd5ef4
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-05-05 04:42:10 -0300

    Exemplos de protoype com função construtora

--- POO/prototype.js
+++ POO/prototype.js
@@ -1,9 +1,43 @@
-let array = [1, 2, 3]
+/* Arrow functions fora da função construtora são problemáticas */
 
-const obj = {
-    name: 'Mateus',
-    lastName: 'Vitor'
+function Person (name, age, sex) {
+    this._name = name
+    this._age = age
+    this._sex = sex
+
+    /* Prototype 1ª forma */
+    this.getName = function () {
+        return this._name
+    }
+
+    this.setName = function (value) {
+        this._name = value
+    }
 }
 
-console.log(array.__proto__)
-console.log(obj.__proto__)
+/* Prototype 2ª forma */
+Person.prototype = {
+    /* Quando o objeto é alterado por prototype, o constructor fica como Object sendo necessário a redefinição */
+    constructor: Person,
+
+    getAge: function () {
+        return this._age
+    },
+    setAge: function (value) {
+        this._age = value
+    }
+}
+
+/* Prototype 3ª forma */
+Person.prototype.getSex = function () {return this._sex}
+Person.prototype.setSex = function (value) {this._sex = value}
+
+let person = new Person('Mateus', 24, 'M')
+person.setName('Mateus Vitor')
+person.setAge(25)
+person.setSex('MM')
+
+console.log(person.getName())
+console.log(person.getAge())
+console.log(person.getSex())
+console.log(person.constructor)
