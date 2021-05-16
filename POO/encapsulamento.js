fa78ab25a55eb3a5966c197b791dc67a1ba2f2ee
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-05-16 00:18:26 -0300

    Polimorfismo JS
    Maneira em desuso de se fazer polimorfismo.

---
+++ POO/encapsulamento.js
@@ -0,0 +1,23 @@
+function Person (name, age) {
+    var _name = name
+    var _age = age
+
+    this.getAge = function () {
+        return _age
+    }
+
+    this.getName = function () {
+        return _name
+    }
+
+    this.setName = function (value) {
+        _name = value
+    }  
+
+
+
+}
+
+let person = new Person('Mateus', 24)
+person.setName('Mateus Vitor')
+console.log(person.getName())
